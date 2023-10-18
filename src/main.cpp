a2a667262d8d774f5875c35d949835702292b5e7
Kevin Kerker <kevin.kerker@msquare.de>
2023-10-18 23:07:51 +0200

    updated lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
-#include <Arduino.h>
+#include "main.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SD.h>
+
+AsyncWebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  SD.begin(5, SPI, 40000000, "/", 5, false);
+  serveLargeStatic(server, "/static/", SD, "/www/static/", "max-age=86.400");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
---
+++ src/main.h
@@ -0,0 +1,8 @@
+#ifndef B6AB01FC_8EDC_4098_87E6_6075B5FD9BD3
+#define B6AB01FC_8EDC_4098_87E6_6075B5FD9BD3
+#include <Arduino.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include "../lib/serveLargeStatic/serveLargeStatic.h"
+#endif /* B6AB01FC_8EDC_4098_87E6_6075B5FD9BD3 */
