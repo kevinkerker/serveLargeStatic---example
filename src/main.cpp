7d377e7c16e77fbc486aeeb8db5dcfbbbaa16c4e
Kevin Kerker <kevin.kerker@msquare.de>
2023-10-25 15:55:24 +0200

    001

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@ AsyncWebServer server(80);
 
 void setup() {
   SD.begin(5, SPI, 40000000, "/", 5, false);
-  serveLargeStatic(server, "/static/", SD, "/www/static/", "max-age=86.400");
+  // server.serveStatic();
+  serveLargeStaticFolder* largeStaticFolder =
+      new serveLargeStaticFolder(server, SD, "/www/static/", "max-age=86.400");
 }
 
 void loop() {
